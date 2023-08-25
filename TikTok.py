1cc32f3f31d1a34183ea03063b6b09a46cc21f2f
JustEnoughKip <143220641+JustEnoughKip@users.noreply.github.com>
2023-08-25 14:31:14 -0500

    First Commit
    Python script to clean up TikTok captions created in CapCut

---
+++ TikTok.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+  #if len(sys.argv) != 2:
+  #  print("Invalid args")
+  #  return
+  #DataPath = sys.argv[1]
+  DataPath = "draft_info.json"
+  NewData = ""
+  with open(DataPath, 'r') as file:
+    data = file.read()
+    file.close()
+    sentences = data.split(">[")
+    for sentence in sentences:
+        sentence = " " + sentence
+        sentence = sentence.replace(" it'll ", " it will ").replace(" wanna ", " want to ").replace(" gonna ", " going to ").replace(" um ", " ").replace(" uh ", " ").replace(" ah ", " ").replace(" so ", " ").replace(" you know ", " ").replace(" like ", " ").replace(" well ", " ")  .replace(" wanna]<", " want to]<").replace(" gonna]<", " going to]<").replace(" um]<", "]<").replace(" uh]<", "]<").replace(" ah]<", "]<").replace(" so]<", "]<").replace(" you know]<", "]<").replace(" like]<", "]<").replace(" well]<", "]<")
+        sentence = sentence.strip()
+        MyCap = sentence[0]
+        MyCap = MyCap.upper()
+        sentence = MyCap + sentence[1:]
+        NewData += sentence + ">["
+
+    NewData = NewData[:-2]
+    with open(DataPath, 'w') as file2:
+        file2.write(NewData)
+    print("complete!")
+
+if __name__ == "__main__":
+  main()
+
